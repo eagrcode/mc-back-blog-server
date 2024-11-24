8d184b0c8cd641694af15aec7ffddc1096b3fb64
Elliot Robinson <elliot.robinson92@hotmail.com>
2024-11-24 15:28:28 +0000

    reconfig app from vercel serverless back to normal express

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Initialise S3 client
+const PORT = process.env.PORT || 5000;
+
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
@@ -18,25 +19,24 @@ const s3Client = new S3Client({
   },
 });
 
-const BUCKET_NAME = process.env.S3_BUCKET_NAME;
-
-app.get("/", async (req, res) => {
-  res.send("Welcome to the server");
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
 });
 
-// Generate presigned URL
-app.post("/generate-presigned-url", async (req, res) => {
+app.post("/get-s3-url", async (req, res) => {
   const { fileName, fileType } = req.body;
-
-  const params = {
-    Bucket: BUCKET_NAME,
-    Key: fileName,
-    ContentType: fileType,
-  };
+  console.log(fileName, fileType);
 
   try {
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: fileName,
+      ContentType: fileType,
+    };
+
     const command = new PutObjectCommand(params);
     const url = await getSignedUrl(s3Client, command, { expiresIn: 60 });
+
     res.status(200).json({ url });
   } catch (error) {
     console.error("Error generating presigned URL:", error);
@@ -44,7 +44,8 @@ app.post("/generate-presigned-url", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
